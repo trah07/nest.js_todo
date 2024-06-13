9c335cf4571ab443bf3ce4bf5c95975273fdfbd1
trah07 <trah07@vse.cz>
2024-06-13 02:45:10 +0200

    graphql backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TodosModule } from "./todos/todos.module";
+import { PubSub } from "graphql-subscriptions";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+
+const pubSub = new PubSub();
 
 @Module({
-  imports: [],
+  imports: [
+    TodosModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      context: ({ req, res }) => ({ req, res, pubSub }),
+      subscriptions: {
+        "subscriptions-transport-ws": {
+          path: "/graphql",
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
