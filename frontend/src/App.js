ca41a990d09287a4d88038636743a3489ef5f00c
trah07 <trah07@vse.cz>
2024-06-13 03:40:31 +0200

    nest js todo v.1.0

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import { gql, useMutation, useQuery, useSubscription } from "@apollo/client";
+import { useEffect, useState } from "react";
+
+const GET_TODOS = gql`
+  query GetTodos {
+    getTodos {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const CREATE_TODO = gql`
+  mutation CreateTodo($title: String!) {
+    createTodo(input: { title: $title }) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const TOGGLE_TODO_COMPLETED = gql`
+  mutation ToggleTodoCompleted($id: String!) {
+    toggleTodoCompleted(id: $id) {
+      id
+      title
+      completed
+    }
+  }
+`;
+
+const TODO_CREATED = gql`
+  subscription TodoCreated {
+    todoCreated {
+      id
+      title
+      completed
+    }
+  }
+`;
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const { data, loading, error } = useQuery(GET_TODOS);
+  const [createTodo] = useMutation(CREATE_TODO);
+  const [toggleTodoCompleted] = useMutation(TOGGLE_TODO_COMPLETED);
+
+  const { data: todoCreated } = useSubscription(TODO_CREATED);
+
+  console.log(todoCreated);
+  useEffect(() => {
+    if (todoCreated) {
+      const newTodo = todoCreated.todoCreated;
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+    }
+  }, [todoCreated]);
+
+  useEffect(() => {
+    if (data) {
+      setTodos(data.getTodos);
+    }
+  }, [data]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error! :(</p>;
+
+  const handleCreateTodo = (e) => {
+    e.preventDefault();
+    if (title) {
+      createTodo({
+        variables: { title: title },
+        update: (cache, { data: { createTodo: todo } }) => {
+          cache.modify({
+            fields: {
+              getTodos(existingTodos = []) {
+                const newTodoRef = cache.writeFragment({
+                  data: todo,
+                  fragment: gql`
+                    fragment NewTodo on Todo {
+                      id
+                      title
+                      completed
+                    }
+                  `,
+                });
+                return [...existingTodos, newTodoRef];
+              },
+            },
+          });
+        },
+      });
+      setTitle("");
+    }
+  };
+
+  const handleTodoCompleted = (id, title) => {
+    toggleTodoCompleted({
+      variables: { id },
+      optimisticResponse: {
+        __typename: "Mutation",
+        toggleTodoCompleted: {
+          __typename: "Todo",
+          id,
+          title,
+          completed: !todos.find((todo) => todo.id === id).completed,
+        },
+      },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="todo-form">
+        <form onSubmit={handleCreateTodo}>
+          <input
+            type="text"
+            placeholder="Enter task title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button type="submit">Add</button>
+        </form>
+      </div>
+      <div className="todo-list">
+        <h2>Todos</h2>
+        <ul>
+          {todos.length > 0 ? (
+            todos.map((todo) => (
+              <li className={todo.completed ? "completed" : ""} key={todo.id}>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => {
+                    handleTodoCompleted(todo.id, todo.title);
+                  }}
+                />
+                <span>{todo.title}</span>
+              </li>
+            ))
+          ) : (
+            <li>
+              <h4>Create a new task</h4>
+            </li>
+          )}
+        </ul>
+      </div>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { WebSocketLink } from "@apollo/client/link/ws";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const httpLink = new HttpLink({
+  uri: "http://localhost:3000/graphql",
+});
+
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:3000/graphql",
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
