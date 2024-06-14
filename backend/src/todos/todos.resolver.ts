f054399b1dee4fe11d9798ccfc7b43305fd85431
trah07 <trah07@vse.cz>
2024-06-15 01:39:28 +0200

    fixing resolver

--- backend/src/todos/todos.resolver.ts
+++ backend/src/todos/todos.resolver.ts
@@ -2,70 +2,69 @@ import { Args, Mutation, Query, Resolver, Subscription } from "@nestjs/graphql";
 import { PubSub } from "graphql-subscriptions";
 import { Todo } from "./todo.entity";
 import { CreateTodoInput, UpdateTodoInput } from "./todo.dto";
+import { NotFoundException } from "@nestjs/common";
 
 const pubSub = new PubSub();
 
 @Resolver(() => Todo)
 export class TodosResolver {
-  todos: Todo[] = [];
+  private todos: Todo[] = [];
 
   @Query(() => [Todo])
-  getTodos() {
+  getTodos(): Todo[] {
     return this.todos;
   }
 
   @Query(() => Todo, { nullable: true })
-  getTodo(@Args("id") id: string) {
-    return this.todos.find((todo) => todo.id === id);
+  getTodo(@Args("id") id: string): Todo {
+    const todo = this.todos.find((todo) => todo.id === id);
+    if (!todo) {
+      throw new NotFoundException("Todo not found");
+    }
+    return todo;
   }
 
   @Mutation(() => Todo)
-  createTodo(@Args("input") input: CreateTodoInput) {
-    const todo = {
+  createTodo(@Args("input") input: CreateTodoInput): Todo {
+    const todo: Todo = {
       id: Date.now().toString(),
       title: input.title,
       completed: false,
     };
     this.todos.push(todo);
-    pubSub.publish("todoCreated", { todoCreated: todo });
+    this.publishEvent("todoCreated", todo);
     return todo;
   }
 
   @Mutation(() => Todo)
-  toggleTodoCompleted(@Args("id") id: string) {
-    const todo = this.todos.find((todo) => todo.id === id);
-    if (!todo) {
-      throw new Error("Todo not found");
-    }
+  toggleTodoCompleted(@Args("id") id: string): Todo {
+    const todo = this.findTodoById(id);
     todo.completed = !todo.completed;
-    pubSub.publish("todoCompleted", { todoCompleted: todo });
+    this.publishEvent("todoCompleted", todo);
     return todo;
   }
 
   @Mutation(() => Todo, { nullable: true })
-  deleteTodo(@Args("id") id: string) {
+  deleteTodo(@Args("id") id: string): Todo {
     const index = this.todos.findIndex((todo) => todo.id === id);
     if (index === -1) {
-      throw new Error("Todo not found");
+      throw new NotFoundException("Todo not found");
     }
     const [deletedTodo] = this.todos.splice(index, 1);
-    pubSub.publish("todoDeleted", { todoDeleted: deletedTodo });
+    this.publishEvent("todoDeleted", deletedTodo);
     return deletedTodo;
   }
 
   @Mutation(() => Todo)
-  updateTodo(@Args("input") input: UpdateTodoInput) {
-    const todo = this.todos.find((todo) => todo.id === input.id);
-    if (!todo) {
-      throw new Error("Todo not found");
-    }
+  updateTodo(@Args("input") input: UpdateTodoInput): Todo {
+    const todo = this.findTodoById(input.id);
     if (input.title !== undefined) {
       todo.title = input.title;
     }
     if (input.completed !== undefined) {
       todo.completed = input.completed;
     }
-    pubSub.publish("todoUpdated", { todoUpdated: todo });
+    this.publishEvent("todoUpdated", todo);
     return todo;
   }
 
@@ -87,5 +86,17 @@ export class TodosResolver {
   @Subscription(() => Todo)
   todoUpdated() {
     return pubSub.asyncIterator("todoUpdated");
+  }
+
+  private findTodoById(id: string): Todo {
+    const todo = this.todos.find((todo) => todo.id === id);
+    if (!todo) {
+      throw new NotFoundException("Todo not found");
+    }
+    return todo;
+  }
+
+  private publishEvent(event: string, payload: Todo) {
+    pubSub.publish(event, { [event]: payload });
   }
 }
